b79361af599691e87bcbd2225aaa622b42cc06fa
Christian Dähn <christian.daehn@gmail.com>
2016-11-06 21:22:20 +0100

    wait 20 seconds after each try

--- uploadr.py
+++ uploadr.py
@@ -662,7 +662,7 @@ class Uploadr:
 
                     count = 0
 
-                    left_photos = self.photos_on_flickr(stored_md5)
+                    left_photos = 1
                     while left_photos > 0:
                         if count > MAX_UPLOAD_ATTEMPTS:
                             raise Exception('file still not deleted after %d attempts' % count, files_id, stored_md5,
@@ -670,6 +670,7 @@ class Uploadr:
                         count += 1
                         print 'waiting for photo md5(%s) to be deleted on flickr...%d left' % (stored_md5, left_photos)
                         time.sleep(20)
+                        left_photos = self.photos_on_flickr(stored_md5)
                     self.upload_file(file_path)
                     assert 1 == self.photos_on_flickr(file_md5)
         return success
